690d4472d43fac0f06dd5b710f3cc31be3d63929
Jonathan Mühlenpfordt <muehlenpfordt@neon-energie.de>
2016-08-09 01:51:01 +0200

    worked on on/offshore issue for German wind data

--- timeseries_scripts/download.py
+++ timeseries_scripts/download.py
@@ -22,14 +22,129 @@ logger = logging.getLogger('log')
 logger.setLevel('INFO')
 
 
-def get_opsd_beta_password():
-    if 'MORPH_OPSD_BETA_PW' in os.environ:
-        password = os.environ['MORPH_OPSD_BETA_PW']
+def download(sources_yaml_path, out_path, start_from_user=None, end_from_user=None, subset=None):
+    """
+    Load YAML file with sources from disk, and download all files for each
+    source into the given out_path. Returns None.
+
+    Parameters
+    ----------
+    sources_yaml_path : str
+        Filepath of sources.yml
+    out_path : str
+        Base download directory in which to save all downloaded files.    
+    start_from_user : datetime.date, optional
+        Start of period for which to download the data.
+    end_from_user : datetime.date, optional
+        End of period for which to download the data
+    subset : list or iterable, optional
+        If given, specifies a subset of data sources to download,
+        e.g.: ['TenneT', '50Hertz'].
+
+    """
+    for name, date in {'end_from_user': end_from_user, 'start_from_user': start_from_user}.items():
+        if date and date > datetime.now().date():
+            logger.info('%s given was %s, must be smaller than %s, '
+                        'we have no data for the future!',
+                        name, date, datetime.today().date())
+            return
+    
+    with open(sources_yaml_path, 'r') as f:
+        sources = yaml.load(f.read())
+
+    # If subset is given, only keep source_name keys in subset
+    if subset is not None:
+        sources = {k: v for k, v in sources.items() if k in subset}
+
+    for source_name, source_dict in sources.items():
+        download_source(source_name, source_dict, out_path, start_from_user, end_from_user)
+        
+
+def download_source(source_name, source_dict, out_path, start_from_user=None, end_from_user=None):
+    """
+    Download all files for source_name as specified by the given
+    source_dict into out_path. Returns None.
+
+    Parameters
+    ----------
+    source_name : str
+        Name of source dataset, e.g. ``TenneT``.
+    source_dict : dict
+        Dictionary of variables and their parameters for the given source.
+    out_path : str
+        Base download directory in which to save all downloaded files.
+    start_from_user : datetime.date, optional
+        Start of period for which to download the data.
+    end_from_user : datetime.date, optional
+        End of period for which to download the data
+
+    """
+    # While OPSD is in beta, we need to supply authentication
+    if source_name == 'OPSD':
+        password = get_opsd_beta_password()
+        session = requests.session()
+        session.auth = ('beta', password)
     else:
-        password = getpass.getpass('Please enter the beta user password:')
-    return password
+        session = None
 
+    for variable_name, param_dict in source_dict.items():
+        start_server = param_dict['start']
+        end_server = param_dict['end']
+        
+        if end_server == 'recent':
+            end_server = datetime.now().date()        
 
+        if start_from_user:
+            if start_from_user <= start_server:
+                pass # do nothing
+            # elif start_from_user > param_dict['start'] and start_from_user < param_dict['end']:
+            elif start_server < start_from_user < end_server:    
+                start_server = start_from_user  # replace start_server
+            else: 
+                continue  # skip this variable from the source dict, relevant e.g. in Sweden
+
+        if end_from_user:
+            if end_from_user <= start_server:
+                continue # skip this variable from the source dict, relevant e.g. in Sweden
+            # elif end_from_user > param_dict['start'] and end_from_user < param_dict['end']:
+            elif start_server < end_from_user < end_server:
+                end_server = end_from_user  # replace  end_server
+            else: 
+                pass  # do nothing
+                
+                
+        if param_dict['frequency'] in ['complete', 'irregular']:
+            download_file(
+                source_name, variable_name, out_path, param_dict,
+                start=start_server, end=end_server,
+                session=session
+            )
+        
+        else:
+            # The files on the servers usually contain the data for subperiods
+            # of some regular length (i.e. months or yearsavailable
+            # Create lists of start- and enddates of periods represented in
+            # individual files to be downloaded.
+
+            starts = pd.date_range(
+                start=start_server, end=end_server,
+                freq=param_dict['frequency'] + 'S',
+            )
+            ends = pd.date_range(
+                start=start_server, end=end_server,
+                freq=param_dict['frequency'],
+            )
+
+            if len(ends) == 0:
+                ends = pd.DatetimeIndex([end_server])
+    
+            for s, e in zip(starts, ends):
+                download_file(
+                    source_name, variable_name, out_path, param_dict,
+                    start=s, end=e, session=session
+                )
+                
+                
 def download_file(source_name, variable_name, out_path,
                   param_dict, start, end, session=None):
     """
@@ -149,128 +264,13 @@ def download_file(source_name, variable_name, out_path,
         logger.info('There must not be more than one file in: %s. Please check ',
                      container)
 
-
-def download_source(source_name, source_dict, out_path, start_from_user=None, end_from_user=None):
-    """
-    Download all files for source_name as specified by the given
-    source_dict into out_path. Returns None.
-
-    Parameters
-    ----------
-    source_name : str
-        Name of source dataset, e.g. ``TenneT``.
-    source_dict : dict
-        Dictionary of variables and their parameters for the given source.
-    out_path : str
-        Base download directory in which to save all downloaded files.
-    start_from_user : datetime.date, optional
-        Start of period for which to download the data.
-    end_from_user : datetime.date, optional
-        End of period for which to download the data
-
-    """
-    # While OPSD is in beta, we need to supply authentication
-    if source_name == 'OPSD':
-        password = get_opsd_beta_password()
-        session = requests.session()
-        session.auth = ('beta', password)
+        
+def get_opsd_beta_password():
+    if 'MORPH_OPSD_BETA_PW' in os.environ:
+        password = os.environ['MORPH_OPSD_BETA_PW']
     else:
-        session = None
-
-    for variable_name, param_dict in source_dict.items():
-        start_server = param_dict['start']
-        end_server = param_dict['end']
-        
-        if end_server == 'recent':
-            end_server = datetime.now().date()        
-
-        if start_from_user:
-            if start_from_user <= start_server:
-                pass # do nothing
-            # elif start_from_user > param_dict['start'] and start_from_user < param_dict['end']:
-            elif start_server < start_from_user < end_server:    
-                start_server = start_from_user  # replace start_server
-            else: 
-                continue  # skip this variable from the source dict, relevant e.g. in Sweden
-
-        if end_from_user:
-            if end_from_user <= start_server:
-                continue # skip this variable from the source dict, relevant e.g. in Sweden
-            # elif end_from_user > param_dict['start'] and end_from_user < param_dict['end']:
-            elif start_server < end_from_user < end_server:
-                end_server = end_from_user  # replace  end_server
-            else: 
-                pass  # do nothing
-                
-                
-        if param_dict['frequency'] in ['complete', 'irregular']:
-            download_file(
-                source_name, variable_name, out_path, param_dict,
-                start=start_server, end=end_server,
-                session=session
-            )
-        
-        else:
-            # The files on the servers usually contain the data for subperiods
-            # of some regular length (i.e. months or yearsavailable
-            # Create lists of start- and enddates of periods represented in
-            # individual files to be downloaded.
-
-            starts = pd.date_range(
-                start=start_server, end=end_server,
-                freq=param_dict['frequency'] + 'S',
-            )
-            ends = pd.date_range(
-                start=start_server, end=end_server,
-                freq=param_dict['frequency'],
-            )
-
-            if len(ends) == 0:
-                ends = pd.DatetimeIndex([end_server])
-    
-            for s, e in zip(starts, ends):
-                download_file(
-                    source_name, variable_name, out_path, param_dict,
-                    start=s, end=e, session=session
-                )
-
-
-def download(sources_yaml_path, out_path, start_from_user=None, end_from_user=None, subset=None):
-    """
-    Load YAML file with sources from disk, and download all files for each
-    source into the given out_path. Returns None.
-
-    Parameters
-    ----------
-    sources_yaml_path : str
-        Filepath of sources.yml
-    out_path : str
-        Base download directory in which to save all downloaded files.    
-    start_from_user : datetime.date, optional
-        Start of period for which to download the data.
-    end_from_user : datetime.date, optional
-        End of period for which to download the data
-    subset : list or iterable, optional
-        If given, specifies a subset of data sources to download,
-        e.g.: ['TenneT', '50Hertz'].
-
-    """
-    for name, date in {'end_from_user': end_from_user, 'start_from_user': start_from_user}.items():
-        if date and date > datetime.now().date():
-            logger.info('%s given was %s, must be smaller than %s, '
-                        'we have no data for the future!',
-                        name, date, datetime.today().date())
-            return
-    
-    with open(sources_yaml_path, 'r') as f:
-        sources = yaml.load(f.read())
-
-    # If subset is given, only keep source_name keys in subset
-    if subset is not None:
-        sources = {k: v for k, v in sources.items() if k in subset}
-
-    for source_name, source_dict in sources.items():
-        download_source(source_name, source_dict, out_path, start_from_user, end_from_user)
+        password = getpass.getpass('Please enter the beta user password:')
+    return password
 
 
 if __name__ == '__main__':
