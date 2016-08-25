73aeaa292e384f57c2aa2855695a8186a8f41774
Jonathan Mühlenpfordt <muehlenpfordt@neon-energie.de>
2016-08-25 20:47:12 +0200

    created new main-notebook moved the old content to processing.ipynb

--- timeseries_scripts/__init__.py
+++ timeseries_scripts/__init__.py
@@ -1,2 +1,11 @@
+"""
+Open Power System Data
+
+Timeseries Datapackage
+
+__init__.py : required so files in this folder can be imported as by other scripts
+
+"""
+
 from . import download
 from . import read
